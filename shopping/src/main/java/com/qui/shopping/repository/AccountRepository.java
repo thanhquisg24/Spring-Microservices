e3cdeeee5898ec4b7409a9e3b551aa708d481cfa
qui <qui@TTLF5-PC10.example.com>
2018-09-27 16:38:10 +0700

    shopping monolithic commit

---
+++ shopping/src/main/java/com/qui/shopping/repository/AccountRepository.java
@@ -0,0 +1,20 @@
+package com.qui.shopping.repository;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import com.qui.shopping.entity.Account;
+
+@Transactional
+@Repository
+public class AccountRepository {
+  @Autowired
+  private SessionFactory sessionFactory;
+
+  public Account findAccount(String userName) {
+      Session session = this.sessionFactory.getCurrentSession();
+      return session.find(Account.class, userName);
+  }
+}
