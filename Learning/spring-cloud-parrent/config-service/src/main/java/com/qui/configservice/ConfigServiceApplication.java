48b00f7e7c2099d86bac771dc2b585d84723c567
qui <thanhquisg24@gmail.com>
2019-03-12 14:09:55 +0700

    save changes

--- Learning/spring-cloud-parrent/config-service/src/main/java/com/qui/configservice/ConfigServiceApplication.java
+++ Learning/spring-cloud-parrent/config-service/src/main/java/com/qui/configservice/ConfigServiceApplication.java
@@ -2,7 +2,11 @@ package com.qui.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableConfigServer  // ==> Important!!
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServiceApplication {
 
